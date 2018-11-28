490cb9b06c3eaf0426133eb85f5f96adaf1d7f29
khatribimal.mukesh <khatribimal.mukesh@redbus.com>
2018-11-28 10:55:38 +0530

    If Else Condition2

---
+++ double_the_number.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int a=0;
+
+	printf("Ask a value for a:");
+	scanf("%d",&a);
+
+	if (a==1)
+	{
+
+
+		printf("the value of a+1=%d\n",a+1);
+	}
+
+	else if (a==2)
+	{
+	
+
+		printf("the value of a+2=%d\n",a+2);
+	}
+
+	else if (a==3)
+	{
+	
+
+		printf("the value of a+3=%d\n",a+3);
+	}
+
+	return 0;
+}
