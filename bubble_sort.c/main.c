de89ebd3e95d06cd9796f10c07a3bb9e7318afbe
Bimal-Gangawal <bimalgangawal@gmail.com>
2019-05-02 22:02:25 +0530

    Bubble_trouble

---
+++ bubble_sort.c/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,i,c,j,a[50];
+    printf("Enter n:\n");
+    scanf("%d",&n);
+
+    printf("The array elements:\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+
+            if(a[j]>a[j+1])
+            {
+                c=a[j];
+                a[j]=a[j+1];
+                a[j+1]=c;
+            }
+        }
+
+    }
+    printf("the sorted array is:\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",a[i]);
+    }
+    return 0;
+}
